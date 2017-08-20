b784c53640c22d4262120e488fa58e5f1181baf7
aji1234 <30419580+aji1234@users.noreply.github.com>
2017-08-20 10:18:21 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 	   public int add(int number1, int number2) 
    {
      if (number1> number2)
-	   returnaaaa number1 + number2;
+	   return number1 + number2;
 	   else 
 		   return 10101010;
    }
