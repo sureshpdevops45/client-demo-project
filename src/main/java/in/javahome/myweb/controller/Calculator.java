37b204d7a1b6b2f571ae626f7b35a0a3294a6227
sureshpdevops45 <56384034+sureshpdevops45@users.noreply.github.com>
2020-01-10 14:40:54 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -23,5 +23,16 @@ public class Calculator {
    public int div(int number1, int number2) {
       return number1 / number2;
    }
-   
+    public int div(int number1, int number2) {
+      return number1 / number2;
+   }
+	 public int div(int number1, int number2) {
+      return number1 / number2;
+   }
+	 public int div(int number1, int number2) {
+      return number1 / number2;
+   }
+	 public int div(int number1, int number2) {
+      return number1 / number2;
+   }
 }
